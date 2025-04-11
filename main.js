5eef2038c4445387bf5b33e4b262b59b82202cf8
David Lagos <davidtoledo302@gmail.com>
2025-04-11 01:14:00 -0600

    new JS for rps_UI

--- main.js
+++ main.js
@@ -1,104 +1,63 @@
-/* 
-//Computer choice for rock, paper or scissors 
-function getComputerChoice() {
-    //Random number to equal string
-    let randomNumber = Math.floor(Math.random() * 3);
-
-    if (randomNumber === 1) {
-        return "Rock";
-    }
-    else if (randomNumber === 2) {
-        return "Paper";
-    }
-    else {
-        return "Scissors";
-    }
-}
-
-//Human choice
-function getHumanChoice() {
-    let choice = parseInt(prompt("Enter a number: \n1.Rock \n2.Paper \n3.Scissors")); 
-    
-    if (choice === 1) {
-        return "Rock";
-    }
-    else if (choice === 2) {
-        return "Paper";
-    }
-    else if (choice === 3){
-        return "Scissors";
-    }
-}
-
-//Score and round tracking variables
-let humanScore = 0;
+// New logic for rps game
 let computerScore = 0;
+let humanScore = 0;
 let round = 0;
 
-alert("You have 5 rounds to beat the computer at the Rock, Paper and Scissors game");
+// Human Choice function
+function getHumanChoice() {
+    // Rock
+    document.getElementById("rock").addEventListener('click', e => {
+        console.log(e.target.id);
+    });
 
-//Function to play one round
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "Rock" && computerChoice === "Scissors") {
-        console.log("You won! Rock beats Scissors");
-        humanScore++;
-    }
-    else if (humanChoice === "Paper" && computerChoice === "Rock") {
-        console.log("You won! Paper beats Rock");
-        humanScore++;
-    }
-    else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-        console.log("You won! Scissors beats Paper");
-        humanScore++;
-    }
-    else if (computerChoice === "Rock" && humanChoice === "Scissors") {
-        console.log("Computer won! Rock beats Scissors");
-        computerScore++;
-    }
-    else if (computerChoice === "Paper" && humanChoice === "Rock") {
-        console.log("Computer won! Paper beats Rock");
-        computerScore++;
-    }
-     else if (computerChoice === "Scissors" && humanChoice === "Paper") {
-        console.log("Computer won! Scissors beats Paper");
-        computerScore++;
-    }
-    else if (humanChoice === computerChoice) {
-        console.log("Its a tie!");
-    }
+    // Paper
+    document.getElementById("paper").addEventListener('click', e => {
+        console.log(e.target.id);
+    });
+
+    // Scissors
+    document.getElementById("scissors").addEventListener('click', e => {
+        console.log(e.target.id);
+    });
 }
 
-function getInput() {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-}
+// Calling getHumanChoice function 
+getHumanChoice();
 
-// Function to end game and declare winner
-function declareWinner() {
-    if (humanScore > computerScore) {
-        alert(`You beat the Computer! \nYour score is ${humanScore}`);
+// Computer choice function
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
     }
-    else if (computerScore > humanScore) {
-        alert(`The computer beat you! \nThe computer score is ${computerScore}`);
+    else if (randomNumber === 1) {
+        return "paper";
     }
     else {
-        alert(`Its a tie! \nYour score: ${humanScore} \nComputer score: ${computerScore}`);
+        return "scissors";
     }
 }
 
-// Buttons for each selection (rock, paper, scissors)
-const rock = document.createElement('button');
-const paper = document.createElement('button');
-const scissors = document.createElement('button');
+// Calling getComputerChoice function
+getComputerChoice();
 
-// Event listeners for playRound function
+// Play round function
+function playRound(humanSelection, computerSelection) {
+    switch (humanSelection + computerSelection) {
+        case "rockrock":
+            
+    }
+}
 
+// Reset game function 
+function resetGame() {
+    document.getElementById("reset").addEventListener('click', e => {
+        computerScore = 0;
+        humanScore = 0;
+        round = 0;
+        console.clear();
+    }); 
+}
 
-
-
-
-playGame();
-*/
-
-// New JS logic for rps-ui
+resetGame();
