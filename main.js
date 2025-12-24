36198e45b9ab7c7d46e8ded4c82f9b50b48df235
David Lagos <davidtoledo302@gmail.com>
2025-12-24 12:15:11 -0600

    Added function for user choice.

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+// Function to get a random choice for the computer
 function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random() * 3)];
-} 
+}
+
+// Function to get user choice
+function getUserChoice() {
+    let userChoice = prompt("Enter Rock, Paper, or Scissors: ");
+
+    console.log(userChoice);
+    
+}
